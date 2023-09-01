5416fd6372ee29e045cb3e7c7423919de9e1ebfe
TimCreasman <TimCreasman@users.noreply.github.com>
2023-09-01 18:34:05 -0400

    First build of GithubPages version (#15)
    * First build of GithubPages version
    
    * UPdate eslint config

--- esbuild.build.js
+++ esbuild.build.js
@@ -3,39 +3,43 @@ import {build} from 'esbuild';
 import {exec} from 'child_process';
 import importGlobPlugin from 'esbuild-plugin-import-glob';
 
+var args = process.argv.slice(2);
+
+const folder = args[0];
+
 const copyIndexHtml = {
-    name: 'copyIndex',
-    setup() {
-        exec('cp src/index.html dist/index.html', (error, stdout, stderr) => {
-            if (error) {
-                // eslint-disable-next-line no-undef
-                console.log(`error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                // eslint-disable-next-line no-undef
-                console.log(`stderr: ${stderr}`);
-                return;
-            }
-            // eslint-disable-next-line no-undef
-            console.log('copied index.html');
-        });
-    }
+  name: 'copyIndex',
+  setup() {
+    exec('cp src/index.html ' + folder + '/index.html', (error, stdout, stderr) => {
+      if (error) {
+        // eslint-disable-next-line no-undef
+        console.log(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        // eslint-disable-next-line no-undef
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+      // eslint-disable-next-line no-undef
+      console.log('copied index.html');
+    });
+  }
 };
 
 export const buildOptions = {
-    entryPoints: ['src/main.ts'],
-    bundle: true,
-    outfile: 'dist/main.js',
-    sourcemap: true,
-    plugins: [copyIndexHtml, importGlobPlugin.default()],
+  entryPoints: ['src/main.ts'],
+  bundle: true,
+  outfile: folder + '/main.js',
+  sourcemap: true,
+  plugins: [copyIndexHtml, importGlobPlugin.default()],
 };
 
 build(buildOptions).catch(err => {
-    // eslint-disable-next-line no-undef
-    process.stderr.write(err.stderr);
-    // eslint-disable-next-line no-undef
-    process.exit(1);
+  // eslint-disable-next-line no-undef
+  process.stderr.write(err.stderr);
+  // eslint-disable-next-line no-undef
+  process.exit(1);
 });
 
 
