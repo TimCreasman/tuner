7e24bd1bb2c10dc87e0435abaca1cd67198225ab
Tim Creasman <tcreasman18@gmail.com>
2024-05-23 21:09:56 -0400

    Added FontAwesome brand icons Removed unused icons from imported css

---
+++ src/app/utilities/fontawesome-brands.ts
@@ -0,0 +1,17 @@
+import { css } from 'lit-element';
+
+export default css`
+    /*!
+     * Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com
+     * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
+     * Copyright 2024 Fonticons, Inc.
+     */
+
+    /*
+    * This is only a partial list. This was easier than unessecarily loading a ton of unused css.
+    */
+
+    .fa-paypal::before {
+      content: "\\f1ed";
+    }
+`;
