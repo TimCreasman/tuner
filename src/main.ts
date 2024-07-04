30d3f45d5f472d9e555b303df0a1a16321fd39eb
TimCreasman <TimCreasman@users.noreply.github.com>
2024-07-03 22:01:50 -0400

    Added PWA compatibility (#38)
    * Added PWA compatibility
    * Added service worker caching for offline PWA functionality

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,12 @@ import components from './app/components/**/!(*.spec.ts)';
 import './assets/css/main.css';
 
 components[0].default;
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker
+      .register('/serviceWorker.js')
+      .catch(err => console.log('service worker not registered', err));
+  });
+}
+
